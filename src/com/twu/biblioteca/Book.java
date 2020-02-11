e2fb8c9ae7b16c0344b4ac48025f7270dd4b2ad5
Jo Correa <joseline.correa@gmail.com>
2020-02-11 11:11:31 -0300

    Add returnBook method

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -3,9 +3,9 @@ package com.twu.biblioteca;
 import org.jetbrains.annotations.NotNull;
 
 public class Book {
-    String author;
-    int yearPublished;
-    boolean isAvailable = true;
+    private String author;
+    private int yearPublished;
+    private boolean isAvailable = true;
 
     public Book(String author, int yearPublished) {
         this.author = author;
@@ -14,19 +14,28 @@ public class Book {
 
     @Override
     public String toString() {
-        return "Author: " + author + " - Year Published: " + yearPublished ;
+        return author + " - " + yearPublished ;
     }
 
     public boolean isAvailable() {
         return isAvailable;
     }
 
-    public void checkOutBook() {
+    public void checkoutBook() {
         if (this.isAvailable) {
             this.isAvailable = false;
             System.out.println("Thank you! Enjoy the book");
         } else {
             System.out.println("Sorry, that book is not available");
+        }
+    }
+
+    public void returnBook() {
+        if (this.isAvailable) {
+            System.out.println("That is not a valid book to return");
+        } else {
+            this.isAvailable = true;
+            System.out.println("Thank you for returning the book");
         }
     }
 }
--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -9,34 +9,70 @@ import static org.junit.Assert.*;
 
 public class BookTest {
 
-    @Test
-    public void shouldTurnIsAvailableFalse() {
-        Book book = new Book("Book test",2020);
-        book.checkOutBook();
 
-        assertEquals(false, equals(book.isAvailable));
+    @Test
+    public void shouldTurnIsAvailableFalseWhenCheckout() {
+        Book book = new Book("Book test",2020);
+        book.checkoutBook();
+
+        assertEquals(false, book.isAvailable());
     }
 
     @Test
-    public void shouldShowSuccessMessageWhenCheckOutBook() {
+    public void shouldShowSuccessMessageWhenCheckoutBook() {
         final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
         Book book = new Book("Book test",2020);
-        book.checkOutBook();
+        book.checkoutBook();
 
         assertEquals("Thank you! Enjoy the book\n",outContent.toString());
     }
 
     @Test
-    public void shouldShowUnSuccessfulMessageWhenCheckOutBook() {
+    public void shouldShowUnSuccessfulMessageWhenCheckoutBook() {
+        Book book = new Book("Book test", 2020);
+        book.checkoutBook();
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        book.checkoutBook();
+
+        assertEquals("Sorry, that book is not available\n", outContent.toString());
+    }
+
+    @Test
+    public void shouldTurnIsAvailableTrueWhenReturn() {
+        Book book = new Book("Book test",2020);
+        book.checkoutBook();
+        book.returnBook();
+
+        assertEquals(true, book.isAvailable());
+    }
+
+    @Test
+    public void shouldShowSuccessMessageWhenReturnBook() {
+        Book book = new Book("Book test",2020);
+        book.checkoutBook();
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        book.returnBook();
+
+        assertEquals("Thank you for returning the book\n",outContent.toString());
+    }
+
+    @Test
+    public void shouldShowUnSuccessfulMessageWhenReturnBook() {
         final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
         Book book = new Book("Book test", 2020);
-        book.isAvailable = false;
-        book.checkOutBook();
+        book.returnBook();
 
-        assertEquals("Sorry, that book is not available\n", outContent.toString());
+        assertEquals("That is not a valid book to return\n", outContent.toString());
     }
+
 }
